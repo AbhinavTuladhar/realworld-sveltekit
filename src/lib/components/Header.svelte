0510e81f20565c1e13c5e7b560a0bd842c9e535c
Abhinav Tuladhar <tuladharabhinav78@gmail.com>
2026-04-16 11:31:06 +0545

    basic header implement

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,27 +1,41 @@
 <script>
-  const links = [{
-    name: 'Home',
-    href: '/',
-  }, {
-    name: 'Sign in',
-    href: '/login',
-  }, {
-    name: 'Sign up',
-    href: '/register',
-  }]
+	import { page } from '$app/state';
+	import { resolve } from '$app/paths';
+
+	const currentPath = $derived(page.url.pathname);
+
+	const links = [
+		{
+			name: 'Home',
+			href: '/'
+		},
+		{
+			name: 'Sign in',
+			href: '/login'
+		},
+		{
+			name: 'Sign up',
+			href: '/register'
+		}
+	];
 </script>
 
-<header>
-  <div class="container py-6 flex justify-between">
-    <h1 class='text-4xl'> conduit </h1>
-    <nav>
-      <ul class='flex gap-4'>
-        {#each links as link (link.href)}
-          <li>
-            <a href={link.href}>{link.name}</a>
-          </li>
-        {/each}
-      </ul>
-    </nav>
-  </div>
+<header class="border-b border-gray-600">
+	<div class="container flex items-center justify-between py-3">
+		<h1 class="text-4xl">
+			<a href={resolve('/')}> conduit </a>
+		</h1>
+		<nav>
+			<ul class="flex gap-4">
+				{#each links as link (link.href)}
+					<li>
+						<a
+							class={['text-gray-500 duration-150', { 'text-white': currentPath === link.href }]}
+							href={resolve(link.href)}>{link.name}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+	</div>
 </header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,15 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+  <link
+    href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+    rel="stylesheet"
+    type="text/css"
+  />
+</svelte:head>
 <Header />
-{@render children()}
+<main class='container py-10'>
+	{@render children()}
+</main>
