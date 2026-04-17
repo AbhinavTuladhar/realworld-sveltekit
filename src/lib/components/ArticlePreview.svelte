28d6880794aea231ed5b4f18405b24823dc5b3ca
Abhinav Tuladhar <tuladharabhinav78@gmail.com>
2026-04-17 14:37:55 +0545

    feat: make homepage

---
+++ src/lib/components/ArticlePreview.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import type { Article } from '$lib/types';
+
+	const { author, createdAt, description, favoritesCount, slug, tagList, title }: Article =
+		$props();
+</script>
+
+<article>
+	<div class="top-row">
+		<div class="image">
+			<img src={author.image} alt={author.username} />
+		</div>
+		<div class="author-date">
+			<span class="author">{author.username}</span>
+			<span class="date">{createdAt}</span>
+		</div>
+		<button class="favourite">
+			<span class="heart">❤️</span>
+			<span class="count">{favoritesCount}</span>
+		</button>
+	</div>
+	<h2 class="title">{title}</h2>
+	<p class="description">
+		{description}
+	</p>
+	<ul class="tags-list">
+		{#each tagList as tag (tag)}
+			<li class="tag-capsule">{tag}</li>
+		{/each}
+	</ul>
+</article>
+
+<style lang="scss">
+	article {
+		position: relative;
+		padding-block: 1.5rem;
+		border-bottom: 1px solid var(--border);
+	}
+
+	.top-row {
+		display: flex;
+		gap: 0.5rem;
+	}
+
+	.image {
+		aspect-ratio: 1 / 1;
+		width: 2rem;
+		height: 2rem;
+		border-radius: 50%;
+		overflow: hidden;
+	}
+
+	.author-date {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.favourite {
+		margin-left: auto;
+		border: 1px solid hsl(90, 50%, 50%);
+		align-self: flex-start;
+		padding: 0.25em 0.5em;
+		border-radius: 12px;
+		transition: all 0.4s ease-out;
+		cursor: pointer;
+	}
+
+	.favourite .count {
+		mix-blend-mode: difference;
+	}
+
+	.favourite:hover {
+		background-color: hsl(90, 50%, 50%);
+	}
+
+	.title {
+		margin-top: 0.5rem;
+		font-size: 1.5rem;
+		font-weight: bold;
+		line-height: 1.2;
+	}
+
+	.description {
+		color: hsl(0, 0%, 65%);
+	}
+
+	.tags-list {
+		gap: 0.5rem;
+		display: flex;
+		flex-wrap: wrap;
+		margin-left: auto;
+		max-width: 50%;
+		margin-top: 1rem;
+		justify-content: flex-end;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,52 @@
-<script lang='ts'>
-  const { data } = $props();
+<script lang="ts">
+	import ArticlePreview from '$lib/components/ArticlePreview.svelte';
+
+	const { data } = $props();
+
+	const {
+		data: { articles, tags }
+	} = $derived(data);
 </script>
 
-<div>
-  <pre>
-    {JSON.stringify(data, null, 2)}
-  </pre>
+<div class="main-grid">
+	<section class="articles-container">
+		{#each articles as article (article.slug)}
+			<ArticlePreview {...article} />
+		{/each}
+	</section>
+	<aside class="tags-container">
+		{#each tags as tag (tag)}
+			<span class="tag-capsule"> {tag} </span>
+		{/each}
+	</aside>
 </div>
+
+<style>
+	.main-grid {
+		display: grid;
+		grid-template-columns: minmax(0, 1fr) 20rem;
+	}
+
+	.articles-container {
+		border-right: 1px solid var(--border);
+		padding-right: 1.5rem;
+	}
+	.tags-container {
+		align-self: flex-start;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.5rem;
+		padding-left: 2rem;
+	}
+	:global {
+		.tag-capsule {
+			display: inline-flex;
+			align-items: center;
+			padding: 0.25em 1em;
+			border: 1px solid var(--border);
+			border-radius: 100vw;
+			font-size: 0.875rem;
+			line-height: 1;
+		}
+	}
+</style>
